c7e059ecf35a44c25875ca3412aa0bd54954d9f9
Sean Gofus <sean.gofus@advisorsexcel.com>
2015-07-20 22:52:40 -0500

    Basic controller actions.

--- src/AppBundle/Controller/GameController.php
+++ src/AppBundle/Controller/GameController.php
@@ -2,12 +2,55 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\GameMatch;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/", name="game")
+ */
 class GameController extends Controller
 {
-    public function indexAction($name)
+    const UPDATE_TEMPLATE = 'game/update.html.twig';
+
+    /**
+     * @Route("/", name="game_index")
+     */
+    public function indexAction()
     {
-        return $this->render('', array('name' => $name));
+        return $this->update();
+    }
+
+    /**
+     * @Route("/result", name="game_match_result")
+     */
+    public function resultAction()
+    {
+        die('result');
+    }
+
+    /**
+     * @param GameMatch $entity
+     * @return array
+     */
+    private function update(GameMatch $entity = null)
+    {
+        if (!$entity) {
+            $entity = new GameMatch();
+        }
+
+        return $this->get('form.update_handler')->handleUpdate(
+            $entity,
+            $this->get('appbundle_form'),
+            function (GameMatch $entity) {
+                return array(
+                    'route' => 'game_match_result',
+//                    'parameters' => array('id' => $entity->getId())
+                    'parameters' => []
+                );
+            },
+            $createFormTemplate = self::UPDATE_TEMPLATE
+        );
     }
 }
